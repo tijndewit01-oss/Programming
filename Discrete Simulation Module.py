b422abd7bb1e20158128c3276638b57e81ec11d9
TijnDesktop <tijn.dewit01@gmail.com>
2026-05-27 17:28:27 +0200

    Beginning the general overview of the model.

--- Discrete Simulation Module.py
+++ Discrete Simulation Module.py
@@ -1,3 +1,70 @@
-import sympy as sm
+'''Discrete Simulation Module'''
+'''This module is organized into XXX sections. This is written below:
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+#Importing Libraries
+import sympy as sm
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+
+#Global parameters
+
+
+
+
+'''______________  Section 1: Functions  ______________'''
+
+def TVisitorGenerator():
+
+    return 
+
+
+def TVisitor():
+
+    return
+
+def TCarGenerator():
+
+    return
+
+def TCar():
+
+    return
+
+def TShuttlebus():
+    
+    return
+
+def TTicketscan():
+
+    return
+
+
+
+
+#Initialization of the system
+
+
+
+
+'''______________  Section X: Run the simulation  ______________'''
 
